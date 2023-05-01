1aa4ff862f597e5e36cbab4b3ee42bdb41a744d2
Code-Leader <islomov.islombek.dev@gmail.com>
2023-05-01 21:41:41 +0500

    Auther Code

--- news_app/views.py
+++ news_app/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render, get_list_or_404
 from .models import News, Category
 
 def news_list(request):
-    news_list = News.objects.all()
+    news_list = News.published.all()
     context = {
         "news_list": news_list
     }
     return render(request, 'news/news_list.html', context)
 
 
-def news_detail(request, pk):
-    news = get_list_or_404(News, id=pk)
-    
+# def news_detail(request, pk):
+#     news = get_list_or_404(News, id=pk)
