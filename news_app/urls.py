ee492975e5dd6c2fac655092a4da90f910b00717
Code-Leader <islomov.islombek.dev@gmail.com>
2023-05-01 21:57:06 +0500

    Auther Code

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import news_list
+from .views import news_list, news_detail
 
 urlpatterns = [
     path('news/', news_list, name='all_news_list'),
+    path('/<int:pk>/', news_detail, name="news_detail_page")
 ]
